00df9661c92592c2b48eb2f721c7a17742c46c66
DavidkinEvgeny <47575176+DavidkinEvgeny@users.noreply.github.com>
2020-04-15 12:02:30 +0400

    Создал простое приложение Blog, настроил  маршрутизацию.

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, ContactPageView
 
 
 urlpatterns = [
+    path('contact/', ContactPageView.as_view(), name='contact'),
     path('', HomePageView.as_view(), name='home'),
 ]
